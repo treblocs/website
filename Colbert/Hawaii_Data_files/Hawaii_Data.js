30094d02aedcf05e9275d5618ad65fe6cf1d463d
treblocs <stevenlcolbert@gmail.com>
2026-08-09 11:08:28 -1000

    Delete Colbert directory

--- Colbert/Hawaii_Data_files/Hawaii_Data.js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 3.0.4 local-build-20130225
-
-setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('Hawaii_Data_files/Hawaii_DataMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');Widget.onload();performPostEffectsFixups()}
-function onPageUnload()
-{Widget.onunload();}
